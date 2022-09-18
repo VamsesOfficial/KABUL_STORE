2a5e75b38dcbcf2d92d94aa6e3a97a1f0ffb69ed
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-18 20:18:19 +0700

    Update menu-liststore.js

--- plugins/menu-liststore.js
+++ plugins/menu-liststore.js
@@ -22,11 +22,12 @@ const sections = [
 const listMessage = {
   text: info,
   footer: botdate,
+  mentions: await conn.parseMention(tek),
   title: "KABUL STORE M.M💎28K✅",
   buttonText: "Click Here!",
   sections
 }
-await conn.sendMessage(m.chat, listMessage)
+await conn.sendMessage(m.chat, listMessage, { quoted: m, mentions: await conn.parseMention(tek), contextInfo:{ forwardingScore: 99999, isForwarded: true }})
 //conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
 }
 
