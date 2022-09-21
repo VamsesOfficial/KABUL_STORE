fc9cb4b61828077a397713b93ab1d93db52aa87f
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-21 20:36:38 +0700

    Update menu-liststore.js

--- plugins/menu-liststore.js
+++ plugins/menu-liststore.js
@@ -37,86 +37,24 @@ const sections = [
         {title: "📱 • PULSA INDOSA", rowId: ".game29"},
 	{title: "📱 • PULSA  TRI", rowId: ".game30"},
         {title: "📱 • KOUTA SMARTFREN DATA UNLIMITED ", rowId: ".game31"},            
-	
-	
-	    
-  const listMessage = {
+      ]
+    },
+]
+
+const listMessage = {
     text: info,
     footer: botdate,
     mentions: await conn.parseMention(info),
     title: "👑KABUL STORE/READY STOK👑",
-buttonText: "Click Here!",
-  sections
-await conn.sendMessage(m.chat, listMessage, { quoted: m, mentions: await conn.parseMention(info), contextInfo:{ forwardingScore: 99999, isForwarded: true }})
-//conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
-handler.help = ['liststore']
-handler.tags = ['nocategoty']
-handler.command = /^(list|store|liststore|storelist)$/i
-export default handler 
-
-
-
-
-
-
-	
-	
-
-
-
-
-
-
-
-
+    buttonText: "Click Here!",
+    sections
+  }
+  await conn.sendMessage(m.chat, listMessage)
+  //conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
+  }
   
-
-
-
-
-
-
-
-
-
-      
- 
-
+  handler.help = ['liststore']
+  handler.tags = ['nocategoty']
+  handler.command = /^(list|store|liststore|storelist)$/i
   
-	
-
-	
-
-	
-
-	
-        
-        
-
-  
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-		
-
-        ,       
-
-       
-
+  export default handler
