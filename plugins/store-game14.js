84c9003fd99a1b759fc1747c29006606afea051e
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-23 03:09:10 +0700

    Update store-game14.js

--- plugins/store-game14.js
+++ plugins/store-game14.js
@@ -1,19 +1,29 @@
 let handler = async (m, { conn }) => {
-	//-----PRICE
-let ff1 = 'Rp 6.000'
-let ff2 = 'Rp 11.000'
-let ff3 = 'Rp 20.000'
-let ff4 = 'Rp 50.000'
-let ff5 = 'Rp 99.000'
+
+	//-----PRICElet ff1 = 'Rp 6.000'
+
+let ff2 = 'paymet'
+
+let ff3 = 'paymet'
+
+let ff4 = 'paymet'
+
+
 
 let info = `
-     ✨PAYMENT✨
+
+      ✨PAYMENT✨
 *💦DANA : 085212649276*
 *💦OVO  : 081386943282*
 *💦QRIS : (TANYA ADMIN)*
+    *(🎮KABUL STORE)*
 
-     *(🎮KABUL STORE)*
-   
+
+
+     
+
+
+
 
 
 
@@ -24,26 +34,70 @@ let info = `
 
 
 `
+
 const sections = [
+
    {
-	
+
+	title: `${htjava} List Store ✦-------`,
+
+	rows: [
+
+	    {title: "payment dana", rowId: '.order *Top Up:* payment dana • DANA', description: 'PRICE: ' + ff1 + ', payment DANA' },
+
+        {title: "payment ovo", rowId: '.order *Top Up:* payment • OVO', description: 'PRICE: ' + ff2 + ', Payment OVO' },
+
+        {title: "payment qris", rowId: '.order *Top Up:* payment • QRIS', description: 'PRICE: ' + ff3 + ', payment QRIS },
+
+        
+        
         ]
+
     },
+
 ]
 
 const listMessage = {
+
   text: info,
+
   footer: botdate,
+
   mentions: await conn.parseMention(info),
+
   title: wm,
+
   buttonText: "Click Here!",
+
   sections
+
 }
+
 await conn.sendMessage(m.chat, listMessage, { quoted: m, mentions: await conn.parseMention(info), contextInfo:{ forwardingScore: 99999, isForwarded: true }})
+
 //conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
+
 }
 
 handler.help = ['game2']
+
 handler.command = /^(game2)$/i
 
 export default handler
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
