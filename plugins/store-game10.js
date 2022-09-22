09e127d4d66f2ba561320f9aca3dac4a96a6aa8c
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-22 18:04:13 +0700

    Create store-game10.js

---
+++ plugins/store-game10.js
@@ -0,0 +1,25 @@
+let handler = async (m, { conn }) => {
+        //-----PRICE
+let DM1 = 'Rp 896'
+let ff4 = 'Rp 10.000'
+let ff4 = 'Rp 10.000'
+let ff4 = 'Rp 10.000'
+let ff4 = 'Rp 10.000'
+let ff4 = 'Rp 10.000'
+let ff4 = 'Rp 10.000'
+	  
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
