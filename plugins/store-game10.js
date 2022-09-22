a2b01d6b9f81bfbf2b70be371b1b67598d4e8883
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-22 20:47:56 +0700

    Update store-game10.js

--- plugins/store-game10.js
+++ plugins/store-game10.js
@@ -15,9 +15,9 @@ let info = `
  400Million Koin Non VIP  🪙${ff4}
  1Billion Koin Non VIP    🪙${ff5}
  
-*✅VIA ID*
-*✅PROSES 1 - 5 MENIT*
-*✅TANYAKAN STOCK SEBELUM ORDER* 
+✅VIA ID
+✅PROSES 1 - 5 MENIT
+✅TANYAKAN STOCK SEBELUM ORDER
 
 const sections = [
    {
