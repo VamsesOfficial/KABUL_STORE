48a040a13cb15ad0e36e348e42cd996cef72d33f
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-22 18:44:45 +0700

    Update store-game10.js

--- plugins/store-game10.js
+++ plugins/store-game10.js
@@ -1,13 +1,41 @@
 let handler = async (m, { conn }) => {
         //-----PRICE
-let DM1 = 'Rp 896'
-let ff4 = 'Rp 10.000'
-let ff4 = 'Rp 10.000'
-let ff4 = 'Rp 10.000'
-let ff4 = 'Rp 10.000'
-let ff4 = 'Rp 10.000'
-let ff4 = 'Rp 10.000'
+let ff1 = 'Rp 896'
+let ff2 = 'Rp 6.000'
+let ff3 = 'Rp 18.000'
+let ff4 = 'Rp 35.000'
+let ff5 = 'Rp 66.000'
+
+let info = `
+✨PRICELIST CHIP DOMINO ✨
+	
+ 1Million Koin Non VIP    🪙${ff1}
+ 60Million Koin Non VIP   🪙${ff2}
+ 200Million Koin Non VIP  🪙${ff3}
+ 400Million Koin Non VIP  🪙${ff4}
+ 1Billion Koin Non VIP    🪙${ff5}
+ 
+*>VIA ID*
+*>PROSES 1 - 5 MENIT*
+*>TANYAKAN STOCK SEBELUM ORDER* 
+
+const sections = [
+   {
+        title: `${htjava} List Store ✦-------`,
+        rows: [
+  
+    
+         
 	  
+
+	        
+
+	
+
+
+        
+
+	          
 
  
 
