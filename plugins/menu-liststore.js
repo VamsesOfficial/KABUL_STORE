14e41eecf55d634713f5679b13b9a06f26bc8b52
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-23 18:54:33 +0700

    Update menu-liststore.js

--- plugins/menu-liststore.js
+++ plugins/menu-liststore.js
@@ -7,37 +7,37 @@ const sections = [
     title: `${htjava} 👑KABUL STORE/READY STOCK👑 –––––––––·•`,
     rows: [
 	{title: "💲 •  PAYMENT", rowId: ".game1"},
-        {title: "💎 • Top Up FREE FIRE", rowId: ".game2"},
-        {title: "💎 • Top Up DOMINO QIU QIU", rowId: ".game3"},
-        {title: "💎 • Top Up ARENA OF VALOR(AOV)", rowId: ".game4"},
-        {title: "💎 • Top Up POKER TEXAS", rowId: ".game5"},
-        {title: "💎 • Top Up POINT BLANK ZEPETTO", rowId: ".game6"},
+        {title: "💎 • Top Up FREE FIRE", rowId: ".game1"},
+        {title: "💎 • Top Up DOMINO QIU QIU", rowId: ".game2"},
+        {title: "💎 • Top Up ARENA OF VALOR(AOV)", rowId: ".game3"},
+        {title: "💎 • Top Up POKER TEXAS", rowId: ".game4"},
+        {title: "💎 • Top Up POINT BLANK ZEPETTO", rowId: ".game5"},
         {title: "💎 • Top Up BIG ZENY RAGNAROK", rowId: ".game7"},
-        {title: "💎 • Top Up MOBILE LEGEND", rowId: ".game8"},
-        {title: "💎 • Top Up CALL OF DUTY", rowId: ".game9"},
-        {title: "💎 • Top Up MOBILE PUBG", rowId: ".game10"},
-        {title: "💎 • Top Up CHIP DOMINO", rowId: ".game11"},
-        {title: "💎 • Top Up BIG CAT COIN RAGNAROK", rowId: ".game12"},
-        {title: "💎 • Top Up CANDY SAUSAGE MAN", rowId: ".game13"},
-        {title: "📱 • KOUTA AXIS 3-5 HARI", rowId: ".game14"},
-        {title: "📱 • KOUTA AXIS 15 HARI", rowId: ".game15"},       
-        {title: "📱 • KOUTA AXIS 7 HARI", rowId: ".game16"},
-        {title: "📱 • KOUTA AXIS BRONET 30 HARI", rowId: ".game17"},    
-        {title: "📱 • KOUTA AXIS WEST CANNEL", rowId: ".game18"},
-	{title: "📱 • KOUTA AXIS BRONET 60 HARI", rowId: ".game19"},
-        {title: "📱 • KOUTA AXIS OWSEM 30 HARI", rowId: ".game20"},
-        {title: "📱 • KOUTA AXIS X-TRA SOSIAL MEDIA", rowId: ".game21"},       
-        {title: "📱 • KOUTA AXIS PAKET KZL", rowId: ".game22"},
-        {title: "📱 • KOUTA INDOSAT PURE MINI", rowId: ".game23"},
-        {title: "📱 • KOUTA INDOSAT YELLOW NASIONAL", rowId: ".game24"},
-        {title: "📱 • KOUTA INDOSAT YELLOW GIFT", rowId: ".game25"},    
-        {title: "📱 • KOUTA INDOSAT UNLIMITED HARIAN", rowId: ".game26"},
-        {title: "📱 • KOUTA INDOSAT FREEDOM U UNLIMITED", rowId: ".game27"},
-	{title: "📱 • KOUTA INDOSAT FREEDOM INTERNET", rowId: ".game28"},
-        {title: "📱 • PULSA INDOSAT ", rowId: ".game29"},
-        {title: "📱 • PULSA AXIS", rowId: ".game30"},
-	{title: "📱 • PULSA TRI", rowId: ".game31"},
-        {title: "📱 • PULSA TELKOMSEL  ", rowId: ".game32"},
+        {title: "💎 • Top Up MOBILE LEGEND", rowId: ".game7"},
+        {title: "💎 • Top Up CALL OF DUTY", rowId: ".game8"},
+        {title: "💎 • Top Up MOBILE PUBG", rowId: ".game9"},
+        {title: "💎 • Top Up CHIP DOMINO", rowId: ".game10"},
+        {title: "💎 • Top Up BIG CAT COIN RAGNAROK", rowId: ".game11"},
+        {title: "💎 • Top Up CANDY SAUSAGE MAN", rowId: ".game12"},
+        {title: "📱 • KOUTA AXIS 3-5 HARI", rowId: ".game13"},
+        {title: "📱 • KOUTA AXIS 15 HARI", rowId: ".game14"},       
+        {title: "📱 • KOUTA AXIS 7 HARI", rowId: ".game15"},
+        {title: "📱 • KOUTA AXIS BRONET 30 HARI", rowId: ".game16"},    
+        {title: "📱 • KOUTA AXIS WEST CANNEL", rowId: ".game17"},
+	{title: "📱 • KOUTA INDOSAT PURE MINI", rowId: ".game22"},
+        {title: "📱 • PULSA INDOSAT ", rowId: ".game28"},
+        {title: "📱 • PULSA AXIS", rowId: ".game29"},    
+        {title: "📱 • PULSA TRI", rowId: ".game30"},
+        {title: "📱 • PULSA TELKOMSEL  ", rowId: ".game31"},
+        
+            
+        
+        
+	
+        
+        
+	
+        
 	    
       ]
     },
