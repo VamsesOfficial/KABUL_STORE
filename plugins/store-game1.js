6318b6d6c68b5e41de4701596d77003d124f1b88
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-23 18:31:05 +0700

    Update and rename store-game14.js to store-game1.js

--- plugins/store-game1.js
+++ plugins/store-game1.js
@@ -30,6 +30,6 @@ const listMessage = {
 await conn.sendMessage(m.chat, listMessage, { quoted: m, mentions: await conn.parseMention(info), contextInfo:{ forwardingScore: 99999, isForwarded: true }})
 //conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
 }
-handler.help = ['game14']
-handler.command = /^(game14)$/i
+handler.help = ['game1']
+handler.command = /^(game1)$/i
 export default handler
