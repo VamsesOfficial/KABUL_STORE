9ffd8677948eb0e15f6044388644467838aa0d37
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-24 00:33:59 +0700

    Update store-game17.js

--- plugins/store-game17.js
+++ plugins/store-game17.js
@@ -10,10 +10,10 @@ let ff7 = 'Rp 70.000'
 let ff8 = 'Rp 82.000'
 let info = `
 ✨PRICELIST KOUTA AXIS WEST CANNEL✨
-*📱1,5GB 30 Hari:* ${ff1}
+*📱1,5GB 30 Hari:*${ff1}
 *📱3GB  30 Hari:* ${ff2}
-*📱5GB 30 Hari:* ${ff3}
-*📱8GB 30 Hari:* ${ff4}
+*📱5GB 30 Hari:*  ${ff3}
+*📱8GB 30 Hari:*  ${ff4}
 *📱12GB 30 Hari:* ${ff5}
 *📱13GB 30 Hari:* ${ff6}
 *📱16GB 30 Hari:* ${ff7}
@@ -23,13 +23,13 @@ const sections = [
    {
           title: `${htjava} List Store ✦-------`,
          rows: [
-          {title: "1,5G KOUTA", rowId: '.order *Top Up:* 1,5GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff1 + ', 1,5GB KOUTA KOUTA  AXIS WEST CANNEL },
+        {title: "1,5G KOUTA", rowId: '.order *Top Up:* 1,5GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff1 + ', 1,5GB KOUTA KOUTA  AXIS WEST CANNEL },
 	{title: "3GB KOUTA", rowId: '.order *Top Up:* 3GB KOUTA • KOUTA AXIS WEST CANNEL; description: 'PRICE: ' + ff2 + ', 3GB KOUTA KOUTA AXIS WEST CANNEL' },
-{title: "5GB KOUTA", rowId: '.order *Top Up:* 5GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff3 + ', 5GB KOUTA AXIS WEST CANNEL' },
-{title: "8GB KOUTA", rowId: '.order *Top Up:* 8GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff4 + ', 8GB KOUTA KOUTA AXIS WEST CANNEL' },
+        {title: "5GB KOUTA", rowId: '.order *Top Up:* 5GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff3 + ', 5GB KOUTA AXIS WEST CANNEL' },
+        {title: "8GB KOUTA", rowId: '.order *Top Up:* 8GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff4 + ', 8GB KOUTA KOUTA AXIS WEST CANNEL' },
 	{title: "12GB KOUTA", rowId: '.order *Top Up:* 12GB KOUTA • KOUTA AXIS WEST CANNEL; description: 'PRICE: ' + ff5 + ', 12GB KOUTA KOUTA AXIS WEST CANNEL' },
-{title: "13GB KOUTA", rowId: '.order *Top Up:* 13GB KOUTA • KOUTA AXIS WEST CANNEL ', description: 'PRICE: ' + ff6 + ', 13GB KOUTA KOUTA AXIS WEST CANNEL' },
-{title: "16GB KOUTA", rowId: '.order *Top Up:* 16GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff7 + ', 16GB KOUTA KOUTA AXIS WEST CANNEL' },
+        {title: "13GB KOUTA", rowId: '.order *Top Up:* 13GB KOUTA • KOUTA AXIS WEST CANNEL ', description: 'PRICE: ' + ff6 + ', 13GB KOUTA KOUTA AXIS WEST CANNEL' },
+        {title: "16GB KOUTA", rowId: '.order *Top Up:* 16GB KOUTA • KOUTA AXIS WEST CANNEL', description: 'PRICE: ' + ff7 + ', 16GB KOUTA KOUTA AXIS WEST CANNEL' },
 	{title: "24GB KOUTA", rowId: '.order *Top Up:* 24GB KOUTA • KOUTA AXIS WEST CANNEL; description: 'PRICE: ' + ff8 + ', 24GB KOUTA KOUTA AXIS WEST CANNEL' },
       ]   
   },
@@ -45,6 +45,6 @@ const listMessage = {
 await conn.sendMessage(m.chat, listMessage, { quoted: m, mentions: await conn.parseMention(info), contextInfo:{ forwardingScore: 99999, isForwarded: true }})
  //conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
 }
-handler.help = ['game15']
-handler.command = /^(game15)$/i
+handler.help = ['game17']
+handler.command = /^(game17)$/i
 export default handler
