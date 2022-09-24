c1e3c683a6a359548d56ade2ff1fb6f09466a140
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-24 15:19:41 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -18,10 +18,10 @@ wa.me/6282127487538`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/kabull_store",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'WH-BOT-V1',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
