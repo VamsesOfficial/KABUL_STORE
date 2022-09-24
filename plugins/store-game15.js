c36cbc7f8ef228f5db392e6a8e1fcd3959b94f6a
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-24 18:29:47 +0700

    Update store-game15.js

--- plugins/store-game15.js
+++ plugins/store-game15.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
 let ff1 = 'Rp 16.000'
 let ff2 = 'Rp 19.000'
 let ff3 = 'Rp 21.000'
-let ff4 = 'Rp 23.000'
+let ff4 = 'Rp 33.000'
 let info = `
 ✨PRICELIST KOUTA AXIS 7 hari ✨
 *📱2,5GB  7 Hari* ${ff1}
@@ -15,10 +15,10 @@ let info = `
     {
      title: `${htjava} List Store ✦-------`,
      rows: [
-{title: "2,5GB Kouta", rowId: '.order *Top Up:* 1,5GB Kouta • KOUTA AXIS 7 hari', description: 'PRICE: ' + ff1 + ', 1GB Kouta KOUTA AXIS 7 hari' },
-{title: "3GB Kouta", rowId: '.order *Top Up:* 3GB Kouta • KOUTA AXIS 7 hari', description: 'PRICE: ' + ff2 + ', 3GB Kouta KOUTA AXIS 7 hari' },
+  {title: "2,5GB Kouta", rowId: '.order *Top Up:* 1,5GB Kouta • KOUTA AXIS 7 hari', description: 'PRICE: ' + ff1 + ', 1GB Kouta KOUTA AXIS 7 hari' },
+  {title: "3GB Kouta", rowId: '.order *Top Up:* 3GB Kouta • KOUTA AXIS 7 hari', description: 'PRICE: ' + ff2 + ', 3GB Kouta KOUTA AXIS 7 hari' },
   {title: "4,5GB, Kouta", rowId: '.order *Top Up:* 4,5GB Kouta • KOUTA AXIS 7 hari', description: 'PRICE: ' + ff3 + ', 4,5GB Kouta KOUTA AXIS 7 hari' },
- {title: "10GB Kouta", rowId: '.order *Top Up:* 10GB Kouta • KOUTA AXIS 7 hari', description: 'PRICE: ' + ff4 + ', 10GB Kouta KOUTA AXIS 7 hari' },
+  {title: "10GB Kouta", rowId: '.order *Top Up:* 10GB Kouta • KOUTA AXIS 7 hari', description: 'PRICE: ' + ff4 + ', 10GB Kouta KOUTA AXIS 7 hari' },
         ]
    },
 ]
