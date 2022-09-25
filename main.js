7654da75acb55e7283fa1e9a5fcdb8513233f8c8
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-25 23:18:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -160,7 +160,7 @@ global.reloadHandler = async function (restatConn) {
   }
 
   conn.welcome = '*❖━━━━[WELCOME]━━━━❖*\n\n┏––––––━━━━━━━━•\n│@subject\n┣━━━━━━━━┅┅┅\n*Hallo* @user Welcome 👑KABUL STORE || READY👑 Jangan Lupa Baca Deskripsi Group Sebelum Bertindak\nUntuk Melihat Semua Pricelist Pembayaran Ketik.list Atau .liststore Semoga Betah Di Store Kami🙏\n--––-––┅┅ *ᴅᴇsᴄʀɪᴘᴛɪᴏɴ* ┅┅–––---–\n@desc'
-  conn.bye = '*❖━━━━━━[GOOD BYE]━━━━━━❖*\n𝚂𝚊𝚢𝚘𝚗𝚊𝚛𝚊𝚊 kak *@user* 👋😃'
+  conn.bye = '*❖━━━━━━[GOOD BYE]━━━━━━❖*\n*𝚂𝚊𝚢𝚘𝚗𝚊𝚛𝚊𝚊 kak @user* 👋😃'
   conn.spromote = '@user sekarang admin! '
   conn.sdemote = '@user sekarang bukan admin!'
   conn.sDesc = 'Deskripsi telah diubah ke \n@desc'
