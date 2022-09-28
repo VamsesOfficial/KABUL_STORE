a215ef4c0b1a7279b0a3dd6754cc97f08df9945a
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-28 15:39:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -159,7 +159,7 @@ global.reloadHandler = async function (restatConn) {
     conn.ev.off('creds.update', conn.credsUpdate)
   }
 
-  conn.welcome = '*❖━━━━[WELCOME]━━━━❖*\n\n┏––––––━━━━━━━━•\n│@subject\n┣━━━━━━━━┅┅┅\n*Hallo* @user Welcome 👑KABUL STORE || READY👑 Jangan Lupa Baca Deskripsi Group Sebelum Bertindak\nUntuk Melihat Semua Pricelist Pembayaran Ketik.list Atau .liststore Semoga Betah Di Store Kami🙏\n--––-––┅┅ *ᴅᴇsᴄʀɪᴘᴛɪᴏɴ* ┅┅–––---–\n@desc'
+  conn.welcome = '*❖━━━━━[WELCOME]━━━━━❖*\n\n┏––––––━━━━━━━━•\n│@subject\n┣━━━━━━━━┅┅┅\n*Hallo ka* @user\n├[ *ɪɴᴛʀᴏ* ]—\n│ *ɴᴀᴍᴀ:* \n│ *ᴜᴍᴜʀ:* \n│ *ɢᴇɴᴅᴇʀ:* \n--––-––┅┅ *ᴅᴇsᴄʀɪᴘᴛɪᴏɴ* ┅┅–––---–\n@desc'
   conn.bye = '*❖━━━━━━[GOOD BYE]━━━━━━❖*\n*𝚂𝚊𝚢𝚘𝚗𝚊𝚛𝚊𝚊 kak @user* 👋😃'
   conn.spromote = '@user sekarang admin! '
   conn.sdemote = '@user sekarang bukan admin!'
