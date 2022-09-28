f7828bee12ec7004c58000d9d4d01de5616a47a5
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-28 12:38:47 +0700

    Update group-time.js

--- plugins/group-time.js
+++ plugins/group-time.js
@@ -37,7 +37,7 @@ Maka grup akan di buka otomatis 1 jam kemudian.
         }, timeoutset)
   }
   }
-handler.help = ['group <open/close> <number>']
+handler.help = ['grouptime <open/close> <number>']
 handler.tags = ['group']
 handler.command = /^(grouptime|gctime|.)$/i
 
