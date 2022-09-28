96421849ae34a33b19772376e34be4bd8054d2c5
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-28 17:46:45 +0700

    Update group-settings.js

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 `.trim()
     await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group *open / close*']
+handler.help = ['group *open / close / b / t*']
 handler.tags = ['group']
 handler.command = /^(group| )$/i
 
