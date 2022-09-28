82b0daedc72615e6ff1e16513f22a59d7e92af97
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-28 22:06:20 +0700

    Update downloader-tiktok1.js

--- plugins/downloader-tiktok1.js
+++ plugins/downloader-tiktok1.js
@@ -18,7 +18,7 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
 ${description}
 ◈ ━┉┈┄┈┈ ►
 
-script : https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg
+IG : https://instagram.com/kabulsaputra21?igshid=YmMyMTA2M2Y=
 `.trim(), m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
