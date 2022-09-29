cd9e287e615d6af8cf09bcafea8804ea0b45d45f
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-29 17:28:16 +0700

    Create hallo5.js

---
+++ plugins/hallo5.js
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp3/anjay.mp3')
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaType: 2,
+    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
+    title: 'WH-BOT-V1-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.customPrefix = /^(anjay|an|jay|ja)$/i
+handler.command = new RegExp
+export default handler
