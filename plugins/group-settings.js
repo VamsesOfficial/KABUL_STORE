7ebc6e193e43725c250260be8ac99e5e27dbee11
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-09-29 22:11:41 +0700

    Update group-settings.js

--- plugins/group-settings.js
+++ plugins/group-settings.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     let isClose = { // Switch Case Like :v
         'open': 'not_announcement',
-
+       'buka': 'not_announcement',
       'b': 'not_announcement',
-
+      'close': 'announcement',
 	't': 'announcement',
-                        
+         'tutup': 'announcement',               
     }[(args[0] || '')]
     if (isClose === undefined)
         throw `
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 `.trim()
     await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group *open / close / b / t*']
+handler.help = ['group *open / close*']
 handler.tags = ['group']
 handler.command = /^(group)$/i
 
