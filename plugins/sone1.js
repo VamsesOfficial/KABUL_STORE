15e534b8293dcd5f6b9772771437c1ffb7a6ced8
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-01 12:08:00 +0700

    Create sone1.js

---
+++ plugins/sone1.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+    let info = fs.readFileSync('./mp4/bokep1.mp4')
+
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaType: 2,
+    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
+    title: 'WH-BOT-V1-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig  }}})
+}
+handler.customPrefix = /^(bkp1)$/i
+handler.command = new RegExp
+
+export default handler
