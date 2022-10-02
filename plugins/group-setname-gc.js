84379977c7baf6dbac2cd161954734061643943f
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-02 15:56:51 +0700

    Update group-setname-gc.js

--- plugins/group-setname-gc.js
+++ plugins/group-setname-gc.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
     await conn.groupUpdateSubject(m.chat, `${args.join(" ")}`);
-    m.reply('Sukses mengganti nama group')
+    m.reply('Sukses mengganti nama group oleh admin group')
   }
   
-  handler.help = ['setnamegc <text>']
-  handler.tags = ['group']
-  handler.command = /^setnamegc$/i
+  handler.help = ['setnamegc <text>', 'gc <text>']
+  handler.tags = ['group', 'kabul']
+  handler.command = /^setnamegc|gc$/i
   handler.owner = false
   handler.mods = false
   handler.premium = false
