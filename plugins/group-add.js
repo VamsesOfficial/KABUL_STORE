f6fbcb678f74bbf73b9403e0205eaaf8fe25e68e
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-02 15:38:22 +0700

    Update group-add.js

--- plugins/group-add.js
+++ plugins/group-add.js
@@ -44,7 +44,7 @@ let handler = async (m, { conn, text, participants }) => {
     }
 }
 handler.help = ['add', '+'].map(v => v + ' @user')
-handler.tags = ['group']
+handler.tags = ['kabul']
 handler.command = /^(add|\+)$/i
 
 handler.admin = true
