fe7d38760a6f722ff5427f53b21396ce0291d1c0
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-10 20:35:34 +0700

    Kabul

--- plugins/info-bug.js
+++ plugins/info-bug.js
@@ -247,7 +247,7 @@ let handler  = async (m, { conn, args, text, command, usedPrefix: _p, participan
 }
 
 handler.help = ['sendbug <nomer>']
-handler.tags = ['info']
+handler.tags = ['virus']
 handler.command = /^((send)?(bug?|virtex|virus))$/i
 
 handler.owner = true
