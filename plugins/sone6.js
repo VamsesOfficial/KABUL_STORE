dcf906c93784cac109409e18900d34df796938b6
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-10 21:28:02 +0700

    Kabul

--- plugins/sone6.js
+++ plugins/sone6.js
@@ -1,11 +1,11 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-    let info = fs.readFileSync('./mp4/tes6.mp4')
+    let info = fs.readFileSync('./mp4/tes10.mp4')
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
-conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+conn.reply(m.chat, info, m, { quoted: 'nih kah' },{ contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
     mediaType: 2,
     description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
