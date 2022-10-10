905331bdeffb8d73cb8f11672aafc54a8b5cab60
KABULBOTV1 <113540643+KABULBOTV1@users.noreply.github.com>
2022-10-10 21:20:40 +0700

    Kabul

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,9 +5,12 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let str = `*chat aja langsung ke owner nyah*
 
 Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6282127487538`
+Script bot ini gratis bila kalian mau bisa chat owner
+sebelum chat owner bantu subscribe dulu :v
+link yt ⤵️
+https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow
+wa.me/6281386943282
+*SEKIAN TERIMAKASIH*`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
